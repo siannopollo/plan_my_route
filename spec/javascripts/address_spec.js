b22549404420f586946c7b3b87b3275a4dcb79b2
Steve Iannopollo <steve@iannopollo.com>
2011-02-02 00:28:21 -0500

    Moving specs around a little, and starting on the plan

---
+++ spec/javascripts/address_spec.js
@@ -0,0 +1,141 @@
+describe('Address', function() {
+  var route, address;
+  
+  beforeEach(function() {
+    buildDOM('planning');
+    route = new PlanMyRoute.Route($('planning'));
+    address = route.addresses[2]
+  })
+  
+  afterEach(function() {
+    teardownDOM();
+  })
+  
+  it('should know the route', function() {
+    expect(address.route).toBe(route)
+  });
+  
+  it('should have the route geocoder', function() {
+    expect(address.geocoder).toBe(route.geocoder)
+  });
+  
+  it('should have defaults', function() {
+    expect(address.container).toBeDefined()
+    expect(address.start).toEqual(false)
+    expect(address.makeFirstTrigger).not.toBeNull()
+    expect(address.addAddressTrigger).not.toBeNull()
+    expect(address.removeTrigger).not.toBeNull()
+    expect(address.latlng).toBeNull()
+    expect(address.first).toEqual(false)
+    expect(address.error).toEqual(false)
+  });
+  
+  describe('starting address', function() {
+    beforeEach(function() {
+      startingAddress = route.startingAddress
+    })
+    
+    it('should have different defaults', function() {
+      expect(startingAddress.container).toBeDefined()
+      expect(startingAddress.start).toEqual(true)
+      expect(startingAddress.makeFirstTrigger).toBeNull()
+      expect(startingAddress.addAddressTrigger).toBeNull()
+      expect(startingAddress.removeTrigger).toBeNull()
+      expect(startingAddress.latlng).toBeNull()
+      expect(startingAddress.first).toEqual(false)
+      expect(startingAddress.error).toEqual(false)
+    });
+  })
+  
+  it('should be first after click the make first button', function() {
+    expect(address.first).toEqual(false)
+    address.makeFirstTrigger.fireEvent('click')
+    expect(address.first).toEqual(true)
+    address.makeFirstTrigger.fireEvent('click')
+    expect(address.first).toEqual(false)
+  });
+  
+  it('should not allow two addresses to be first at the same time', function() {
+    otherAddress = route.addresses[1]
+    expect(address.first).toEqual(false)
+    expect(otherAddress.first).toEqual(false)
+    
+    address.makeFirstTrigger.fireEvent('click')
+    expect(address.first).toEqual(true)
+    expect(otherAddress.first).toEqual(false)
+    
+    otherAddress.makeFirstTrigger.fireEvent('click')
+    expect(address.first).toEqual(false)
+    expect(otherAddress.first).toEqual(true)
+  });
+  
+  it('should add a new address to the form', function() {
+    expect($$('.addresses input').length).toEqual(4)
+    expect(route.addresses.length).toEqual(5)
+    
+    address.addAddressTrigger.fireEvent('click')
+    expect($$('.addresses input').length).toEqual(5)
+    expect(route.addresses.length).toEqual(6)
+  });
+  
+  it('should be removed from the form', function() {
+    expect($$('.addresses input').length).toEqual(4)
+    expect(route.addresses.length).toEqual(5)
+    
+    address.removeTrigger.fireEvent('click')
+    expect($$('.addresses input').length).toEqual(3)
+    expect(route.addresses.length).toEqual(4)
+  });
+  
+  it('should always leave at least one address plus the starting address', function() {
+    for (var i = 0; i < 5; i++) {
+      var removedAddress = route.addresses[1]
+      
+      removedAddress.removeTrigger.fireEvent('click')
+      expect($$('.addresses input').length).toBeGreaterThan(0)
+      expect(route.addresses.length).toBeGreaterThan(1)
+    }
+  });
+  
+  it('should have text', function() {
+    expect(address.hasText()).toEqual(false)
+    
+    address.element.set('value', 'something')
+    expect(address.hasText()).toEqual(true)
+    
+    address.element.set('value', address.element.get('placeholder'))
+    expect(address.hasText()).toEqual(false)
+  });
+  
+  it('should retrieve coordinates from the text', function() {
+    expect(address.latlng).toBeNull()
+    
+    address.retrieveCoordinates()
+    expect(address.latlng).toBeNull()
+    
+    address.element.set('value', '1600 Pennsylvania Avenue NW, Washington, DC')
+    runs(function() {
+      address.retrieveCoordinates()
+    })
+    waits(500)
+    runs(function() {
+      expect(address.latlng).not.toBeNull()
+      expect(address.latitude).toEqual(38.8976964)
+      expect(address.longitude).toEqual(-77.0365191)
+    })
+  });
+  
+  it('should mark the address with an error if coordinates cannot be gotten from the text', function() {
+    address.element.set('value', 'udsuidpbwbfi')
+    runs(function() {
+      address.retrieveCoordinates()
+    })
+    waits(500)
+    runs(function() {
+      expect(address.latlng).toBeNull()
+      expect(address.latitude).toBeNull()
+      expect(address.longitude).toBeNull()
+      expect(address.error).toEqual(true)
+    })
+  });
+})
---
+++ spec/javascripts/plan_spec.js
@@ -0,0 +1,32 @@
+describe('Plan', function() {
+  var route;
+  
+  beforeEach(function() {
+    buildDOM('planning');
+    route = new PlanMyRoute.Route($('planning'));
+    setDestinations()
+  })
+  
+  var setDestinations = function() {
+    route.startingAddress.element.set('value', '2050 Clark ave, Raleigh, NC 27605')
+    var addresses = route.addresses.slice(1,5),
+        destinations = []
+    destinations.push('4625 Mill Rock Lane Raleigh NC') // #3 destination
+    destinations.push('4204 Reavis Road, Raleigh, NC 27606') // #1 destination
+    destinations.push('1812 Trinity Rd. Raleigh, NC 27607') // #2 destination
+    destinations.each(function(destination) {
+      var index = Math.floor(Math.random() * addresses.length)
+      var address = addresses[index];
+      address.element.set('value', destination)
+      addresses.erase(address)
+    })
+  }
+  
+  afterEach(function() {
+    // teardownDOM();
+  })
+  
+  it('should do something', function() {
+    console.log('do it')
+  });
+})
